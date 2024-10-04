e0f190fbda49ece62ebd368eb7848a14ae2e5833
Bonnie <bonnieyl.luo@mail.utoronto.ca>
2024-10-03 21:35:49 -0400

    Connect the database

--- project/app.py
+++ project/app.py
@@ -5,6 +5,7 @@ from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
+import os
 
 def login_required(f):
     @wraps(f)
@@ -25,6 +26,13 @@ PASSWORD = "admin"
 SECRET_KEY = "change_me"
 SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+url = os.getenv('DATABASE_URL', f'sqlite:///{Path(basedir).joinpath(DATABASE)}')
+
+if url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = url
 
 # create and initialize a new Flask app
 app = Flask(__name__)
